955a965b71671581227e86b206ee20e2da763a05
Sufiane Souissi <sufiane.souissi@ekino.com>
2018-01-09 18:06:32 +0100

    feat(bot): create bot to tweet my gf

---
+++ index.js
@@ -0,0 +1,65 @@
+const process = require('process')
+const twitter = require('twitter')
+
+// load the .env files variables
+require('dotenv').config()
+
+const twitterConf = {
+    consumer_key: process.env.CONSUMER_KEY,
+    consumer_secret: process.env.CONSUMER_SECRET,
+    access_token_key: process.env.ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+}
+
+// CONSTANTS
+const interval = 5*60*1000
+const usernameToSendTo = process.env.USERNAME_TO_SEND_TO
+const pathToPost = 'statuses/update'
+
+
+const twitterApi = new twitter(twitterConf)
+
+const messageList = [
+    `pocoyo pocoyo ! @${usernameToSendTo}`,
+    `Hola ! @${usernameToSendTo}`,
+    `Mademoiselle ... ! @${usernameToSendTo}`,
+]
+
+/**
+ *  Select a random message from messageList
+ *
+ * @returns {string} - Return a message randomly selected
+ */
+const selectRandomMessages = () => {
+    const randomIndex = Math.floor(Math.random() * messageList.length)
+
+    return messageList[randomIndex]
+}
+
+/**
+ * Send the randomly selected message
+ */
+const sendMessage = () => {
+    const tweetParams = {
+        status: selectRandomMessages(),
+    }
+
+    twitterApi.post(pathToPost, tweetParams, (err) => {
+        if (err) {
+            console.log('---- errooooooor', err)
+        }
+    })
+}
+
+/**
+ * Pocoyo start function
+ *
+ * we assure that as the start Pocoyo send a message
+ */
+const start = () => {
+    sendMessage()
+    setInterval(sendMessage, interval)
+}
+
+// Starting Pocoyo
+start()
